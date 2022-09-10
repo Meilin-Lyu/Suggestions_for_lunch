dfc3d19b08a4a1db529087b97854b48b2354fd7a
Ivan Zhang <ivan@IvandeMacBook-Air.local>
2022-09-10 15:47:31 -0600

    removed old frontend folder

--- frontend/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Home } from "./Home";
-export { default as NoMatch } from "./NoMatch";
-export { default as CreatePost } from "./CreatePost";
-export { default as Categories } from "./Categories";
