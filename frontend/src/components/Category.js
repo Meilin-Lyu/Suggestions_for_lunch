6fe3b37296c05739428f5cd70424c978cdba75d4
Ivan Zhang <ivan@IvandeMacBook-Air.local>
2022-09-11 09:57:11 -0600

    added filter and image display

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -3,15 +3,26 @@ import "../css/Category.css";
 import ToggleButton from 'react-bootstrap/ToggleButton'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 
-const Category = () => {
-    const foodCategory = ['Chinese Food', 'Japanese Food', 'French Food', 'Canadian Food'];
+const Category = (props) => {
+    const categoryName = ['Asian Food', 'Western Food', 'Indian Food'];
+    const category = ['asian', 'western', 'indian'];
     const [value, setValue] = useState();
-    const handleChange = val => setValue(val);
+    const handleChange = val => {
+        setValue(val);
+        props.categoryChange(val);
+    }
     return (
         <div className="Category">
             <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
-                {foodCategory.map((tab, id) => (
-                    <ToggleButton key={id} id={tab} variant="outline-success" value={id}>{tab}</ToggleButton>
+                {categoryName.map((name, id) => (
+                    <ToggleButton
+                        key={id}
+                        id={id}
+                        variant="outline-success"
+                        value={category[id]}
+                        checked={true}>
+                        {name}
+                    </ToggleButton>
                 ))}
             </ToggleButtonGroup>
         </div>
